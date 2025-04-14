dd033be8cfd2cd876be998b2f892165d76a248f9
RootCellar <RootCellar9877@gmail.com>
2025-04-14 10:06:52 -0800

    feat: generate HTML page using arrays from Kotlin code

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -11,6 +11,23 @@ import react.dom.html.ReactHTML.img
 import react.dom.client.createRoot
 import kotlinx.serialization.Serializable
 
+data class Video(
+    val id: Int,
+    val title: String,
+    val speaker: String,
+    val videoUrl: String
+)
+
+val unwatchedVideos = listOf(
+    Video(1, "Opening Keynote", "Andrey Breslav", "https://youtu.be/PsaFVLr8t4E"),
+    Video(2, "Dissecting the stdlib", "Huyen Tue Dao", "https://youtu.be/Fzt_9I733Yg"),
+    Video(3, "Kotlin and Spring Boot", "Nicolas Frankel", "https://youtu.be/pSiZVAeReeg")
+)
+
+val watchedVideos = listOf(
+    Video(4, "Creating Internal DSLs in Kotlin", "Venkat Subramaniam", "https://youtu.be/JzTeAM8N1-o")
+)
+
 fun main() {
     val container = document.getElementById("root") ?: error("Couldn't find root container!")
     createRoot(container).render(Fragment.create {
@@ -21,20 +38,18 @@ fun main() {
             h3 {
                 +"Videos to watch"
             }
-            p {
-                + "John Doe: Building and breaking things"
-            }
-            p {
-                +"Jane Smith: The development process"
-            }
-            p {
-                +"Matt Miller: The Web 7.0"
+            for (video in unwatchedVideos) {
+                p {
+                    +"${video.speaker}: ${video.title}"
+                }
             }
             h3 {
                 +"Videos watched"
             }
-            p {
-                +"Tom Jerry: Mouseless development"
+            for (video in watchedVideos) {
+                p {
+                    +"${video.speaker}: ${video.title}"
+                }
             }
         }
         div {
