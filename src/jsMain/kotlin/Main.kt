917aa8e9cdfc41f9b1d29124e4e7a11c1d7743ff
RootCellar <RootCellar9877@gmail.com>
2025-04-14 10:04:07 -0800

    feat: more rendered HTML from Kotlin, following the tutorial

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -15,7 +15,35 @@ fun main() {
     val container = document.getElementById("root") ?: error("Couldn't find root container!")
     createRoot(container).render(Fragment.create {
         h1 {
-            +"Hello, React+Kotlin/JS!"
+            +"KotlinConf Explorer"
+        }
+        div {
+            h3 {
+                +"Videos to watch"
+            }
+            p {
+                + "John Doe: Building and breaking things"
+            }
+            p {
+                +"Jane Smith: The development process"
+            }
+            p {
+                +"Matt Miller: The Web 7.0"
+            }
+            h3 {
+                +"Videos watched"
+            }
+            p {
+                +"Tom Jerry: Mouseless development"
+            }
+        }
+        div {
+            h3 {
+                +"John Doe: Building and breaking things"
+            }
+            img {
+                src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
+            }
         }
     })
 }
