3aa3d8510d652e649ac3d1a94edf4c06845784b2
RootCellar <RootCellar9877@gmail.com>
2025-04-14 10:19:46 -0800

    refactor: extract HTML page to App component

---
+++ src/jsMain/kotlin/App.kt
@@ -0,0 +1,52 @@
+import kotlinx.coroutines.async
+import react.*
+import react.dom.*
+import kotlinx.browser.window
+import kotlinx.coroutines.*
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import emotion.react.css
+import csstype.Position
+import csstype.px
+import react.dom.html.ReactHTML.h1
+import react.dom.html.ReactHTML.h3
+import react.dom.html.ReactHTML.div
+import react.dom.html.ReactHTML.p
+import react.dom.html.ReactHTML.img
+
+val App = FC<Props> {
+    h1 {
+        +"KotlinConf Explorer"
+    }
+    div {
+        h3 {
+            +"Videos to watch"
+        }
+        for (video in unwatchedVideos) {
+            p {
+                +"${video.speaker}: ${video.title}"
+            }
+        }
+        h3 {
+            +"Videos watched"
+        }
+        for (video in watchedVideos) {
+            p {
+                +"${video.speaker}: ${video.title}"
+            }
+        }
+    }
+    div {
+        css {
+            position = Position.absolute
+            top = 10.px
+            right = 10.px
+        }
+        h3 {
+            +"John Doe: Building and breaking things"
+        }
+        img {
+            src = "https://dummyimage.com/600x400/000000/ffffff&text=Video+Player+Placeholder"
+        }
+    }
+}
--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -30,40 +30,5 @@ val watchedVideos = listOf(
 
 fun main() {
     val container = document.getElementById("root") ?: error("Couldn't find root container!")
-    createRoot(container).render(Fragment.create {
-        h1 {
-            +"KotlinConf Explorer"
-        }
-        div {
-            h3 {
-                +"Videos to watch"
-            }
-            for (video in unwatchedVideos) {
-                p {
-                    +"${video.speaker}: ${video.title}"
-                }
-            }
-            h3 {
-                +"Videos watched"
-            }
-            for (video in watchedVideos) {
-                p {
-                    +"${video.speaker}: ${video.title}"
-                }
-            }
-        }
-        div {
-            css {
-                position = Position.absolute
-                top = 10.px
-                right = 10.px
-            }
-            h3 {
-                +"John Doe: Building and breaking things"
-            }
-            img {
-                src = "https://dummyimage.com/600x400/000000/ffffff&text=Video+Player+Placeholder"
-            }
-        }
-    })
+    createRoot(container).render(App.create())
 }
